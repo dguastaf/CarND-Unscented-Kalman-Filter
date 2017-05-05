bf138b9faaf5e9a43b6de45fa3f8d0ce7d772aea
Doug Guastaferro <dguastaferro@gmail.com>
2017-05-04 20:24:29 -0700

    Tuned hyperparams to meet project goals

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -23,11 +23,11 @@ UKF::UKF() {
 
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
-  P_ << 1, 0, 0,    0,    0,
-		    0, 1, 0,    0,    0,
-		    0, 0, 1, 0,    0,
-		    0, 0, 0,    1, 0,
-        0, 0, 0,    0,    1;
+  P_ << 1, 0, 0, 0,  0,
+		    0, 1, 0, 0,  0,
+		    0, 0, 1, 0,  0,
+		    0, 0, 0, 10, 0,
+        0, 0, 0, 0,  10;
   
   // state dimension
   n_x_ = 5;
@@ -46,13 +46,13 @@ UKF::UKF() {
   std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = .7;
+  std_yawdd_ = .8;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
 
   // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
+  std_laspy_ = 0.10;
 
   // Radar measurement noise standard deviation radius in m
   std_radr_ = 0.3;
@@ -62,14 +62,6 @@ UKF::UKF() {
 
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
-
-  /**
-  TODO:
-
-  Complete the initialization. See ukf.h for other member properties.
-
-  Hint: one or more values initialized above might be wildly off...
-  */
 }
 
 UKF::~UKF() {}
@@ -79,13 +71,7 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Make sure you switch between lidar and radar
-  measurements.
-  */
-  
+ 
   if (!is_initialized_) {
     if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
       float ro = meas_package.raw_measurements_[0];
@@ -106,11 +92,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     return;
   }
   
-  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
-    return;
-  }
-  
-  float delta_t = (meas_package.timestamp_ - time_us_) / 1000000;
+  float delta_t = (meas_package.timestamp_ - time_us_) / 1000000.0;
   
   Prediction(delta_t);
   
@@ -129,24 +111,6 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  * measurement and this one.
  */
 void UKF::Prediction(double delta_t) {
-  /**
-  TODO:
-
-  Complete this function! Estimate the object's location. Modify the state
-  vector, x_. Predict sigma points, the state, and the state covariance matrix.
-  */
-  
-  //Predict sigma points
-  
-  MatrixXd A = P_.llt().matrixL();
-  
-//  Xsig_pred_.col(0) = x_;
-//  
-//  for (int i = 0; i < n_x_; i++)
-//  {
-//    Xsig_pred_.col(i+1) = x_ + sqrt(lambda_ + n_x_) * A.col(i);
-//    Xsig_pred_.col(i+1 + n_x_) = x_ - sqrt(lambda_ + n_x_) * A.col(i);
-//  }
   
   // augment state 
   VectorXd x_aug = VectorXd(n_aug_);
@@ -246,15 +210,7 @@ void UKF::Prediction(double delta_t) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Use lidar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
-
-  You'll also need to calculate the lidar NIS.
-  */
-  
+ 
   VectorXd z = meas_package.raw_measurements_;
   
   MatrixXd H = MatrixXd(2, 5);
@@ -278,6 +234,8 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 	long x_size = x_.size();
 	MatrixXd I = MatrixXd::Identity(x_size, x_size);
 	P_ = (I - K * H) * P_;
+  
+  NIS_laser_ = y.transpose() * Si * y;
 }
 
 /**
@@ -285,14 +243,6 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
  * @param {MeasurementPackage} meas_package
  */
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
-  /**
-  TODO:
-
-  Complete this function! Use radar data to update the belief about the object's
-  position. Modify the state vector, x_, and covariance, P_.
-
-  You'll also need to calculate the radar NIS.
-  */
   
   int n_z = 3;
   MatrixXd Zsig = MatrixXd(n_z, 2 * n_aug_ + 1);
@@ -376,9 +326,5 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
   x_ = x_ + K * z_diff;
   P_ = P_ - K*S*K.transpose();
   
-}
-
-double UKF::NIS(VectorXd z_diff, MatrixXd S) {
-  MatrixXd foo = z_diff.transpose() * S.inverse() * z_diff;
-  return 0;
+  NIS_radar_ = z_diff.transpose() * S.inverse() * z_diff;
 }
